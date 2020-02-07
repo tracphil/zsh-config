df16109a403c728ad802b47ff2f855130f9a43f2
Tracy Phillips <tracphil@outlook.com>
2020-02-07 12:37:26 -0500

    Changing aliases around.

--- lib/alias.zsh
+++ lib/alias.zsh
@@ -5,13 +5,10 @@ export PROJECTS_DIR=$HOME/Projects
 # Mantoso
 export MANTOSO_HOME=$PROJECTS_DIR/mantoso
 
-# Nocbot
-export NOCBOT_HOME=$PROJECTS_DIR/nocbot
-export NOCBOT_FORMULAS_PATH=$NOCBOT_HOME/formulas
-export NOCBOT_ROLES_PATH=$NOCBOT_HOME/roles
-
 # Ansible
-ANSIBLE_ROLES_PATH=$NOCBOT_ROLES_PATH
+export ANSIBLE_HOME=$PROJECTS_DIR/ansible
+export ANSIBLE_CONFIG=$ANSIBLE_HOME/ansible.cfg
+export ANSIBLE_ROLES_PATH=$ANSIBLE_HOME/roles
 
 # GRC colorizes nifty unix tools all over the place
 if which gls >/dev/null 2>&1;
@@ -30,17 +27,15 @@ alias ssh="ssh -A"
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 
 # Directory alias'
-alias cda="cd $PROJECTS_DIR/mantoso/azure"
+alias cda="cd $ANSIBLE_HOME"
 alias cdac="cd $PROJECTS_DIR/mantoso/aws/cloudformation"
 alias cdd="cd $PROJECTS_DIR/mantoso/mantoso-docs"
-alias cdf="cd $NOCBOT_FORMULAS_PATH"
 alias cdg="cd $PROJECTS_DIR/github"
-alias cdm="cd $PROJECTS_DIR/mantoso/"
-alias cdn="cd $NOCBOT_HOME"
+alias cdm="cd $MANTOSO_HOME"
 alias cdo="cd $PROJECTS_DIR/occs/"
 alias cdot="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Settings/dotfiles"
 alias cdp="cd $PROJECTS_DIR"
-alias cdr="cd $NOCBOT_ROLES_PATH"
+alias cdr="cd $ANSIBLE_ROLES_PATH"
 alias cds="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Settings/"
 alias cdt="cd $PROJECTS_DIR/mantoso/azure/mantoso-terraform-azure"
 alias cdtm="cd $PROJECTS_DIR/mantoso/azure/modules"
